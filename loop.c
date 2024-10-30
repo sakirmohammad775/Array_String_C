26b413371b1105db70dd6a2e7fc67640eca002da
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-10-30 15:30:45 +0600

    add Q digit problrm

--- loop.c
+++ loop.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
-#include <ctype.h>
 int main()
 {
-    int A,B,C;
-    
+    int t;
+    scanf("%d", &t);
+    for (int i = 0; i < t; i++)
+    {
+        int n; 
+        scanf("%d", &n);
+        if(n==0){
+            printf("0");
+        }
+        while (n > 0)
+        {
+            int digit = n % 10;
+            printf("%d ", digit);
+            n /= 10;
+        }
+        printf("\n");
+    }
     return 0;
 }
