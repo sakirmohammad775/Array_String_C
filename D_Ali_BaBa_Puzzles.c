34ffdab4454db34073493e192a93b2fc5df7d624
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-10-30 16:37:17 +0600

    ali baba puzzlees

---
+++ D_Ali_BaBa_Puzzles.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+int main()
+{
+    int a, b, c, d;
+    scanf("%d %d %d  %d", &a, &b, &c, &d);
+
+    if (a + b - c == d)
+    {
+        printf("YES");
+    }
+    else if (a + b * c == d)
+    {
+        printf("YES");
+    }
+    else if (a - b * c == d)
+    {
+        printf("YES");
+    }
+    else if (a - b + c == d)
+    {
+        printf("YES");
+    }
+    else if (a * b + c == d)
+    {
+        printf("YES");
+    }
+    else if (a * b - c == d)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+    return 0;
+}
