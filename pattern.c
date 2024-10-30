c31dea9e51a0758f3f6d6f8ab6e7ef6ae88ed770
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-10-30 16:04:19 +0600

    add pattern

--- pattern.c
+++ pattern.c
@@ -5,9 +5,22 @@ int main()
     scanf("%d", &n);
     for (int i = 1; i <= n; i++)
     {
+        for(int j = 1; j <= n-i; j++){
+            printf(" ");
+        }
         for (int j = 1; j <= i; j++)
         {
-            printf("%d ", j);
+            printf("%d ",j);
+        }
+        printf("\n");
+    }
+
+
+
+    //for reverse
+    for(int i = n; i >=0; i--){
+        for(int j=i; j >>= 0; j--){
+            printf("*");
         }
         printf("\n");
     }
