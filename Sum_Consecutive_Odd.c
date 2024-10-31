a5941804f3cc715a76fc9fd8eeb18944ee838025
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-10-31 21:55:11 +0600

    add sum

--- Sum_Consecutive_Odd.c
+++ Sum_Consecutive_Odd.c
@@ -8,13 +8,19 @@ int main()
         int x, y;
         scanf("%d %d", &x, &y);
 
-        
+        if (x > y)
+        {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
 
         int sum = 0;
 
-        for (int i = x+1; i < y; i++)
+        for (int i = x + 1; i < y; i++)
         {
-            if(i%2!=0){
+            if (i % 2 != 0)
+            {
                 sum += i;
             }
         }
