a106dca3e5211d5682b8defe30ef2e01b538596f
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-10-31 10:40:33 +0600

    add sum comprehesive

---
+++ Sum_Consecutive_Odd.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    for (int cs = 0; cs < t; cs++)
+    {
+        int x, y;
+        scanf("%d %d", &x, &y);
+
+        if(x>y){
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+
+        int sum = 0;
+
+        for (int i = x+1; i < y; i++)
+        {
+            if(i%2!=0){
+                sum += i;
+            }
+        }
+        printf("%d\n", sum);
+    }
+
+    return 0;
+}
