fd0150c9db5c9ea80f461c4b0c685c901377fc6b
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-11-01 22:10:19 +0600

    add no repeatation

---
+++ Difference.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+    int A, B;
+    scanf("%d %d", &A, &B);
+
+    int *root1 = &A;
+    int *root2 = &B;
+
+    int difference = abs(*root1 - *root2);
+    printf("%d", difference);
+    return 0;
+}
+/*
+#include <stdio.h>
+int main()
+{
+    int A, B;
+    scanf("%d %d", &A, &B);
+
+    int *root1 = &A;
+    int *root2 = &B;
+
+    int difference = abs(*root1 - *root2);
+    printf("%d", difference);
+    return 0;
+}
+*/
