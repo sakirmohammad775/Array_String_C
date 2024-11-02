ad7abe40b1da7202749bfda78e5da767a8f7556e
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-11-02 18:52:50 +0600

    add new insertion

--- Array_Insertion.c
+++ Array_Insertion.c
@@ -1,6 +1,26 @@
-#include<stdio.h>
-int a[100]
-int main(){
-    int
-return 0;
+#include <stdio.h>
+int a[100000];
+int main()
+{
+    int length;
+    scanf("%d", &length);
+    for (int i = 0; i < length; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    int index,value;
+    scanf("%d %d", &index, &value);
+    
+    length++;
+    for(int i=length; i >=index; i--){
+        a[i+1]=a[i];
+    }
+    a[index]=value;
+
+    for(int i=0; i < length; i++){
+        printf("%d ", a[i]);
+    }
+
+    return 0;
 }
