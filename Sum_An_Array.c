297f59c9a89b19a2ad2368e055287c9ba692b774
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-11-04 13:21:12 +0600

    add Recursion

--- Sum_An_Array.c
+++ Sum_An_Array.c
@@ -1,20 +1,35 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int a[100000],sum;
-void goToIndex(int i,int n){
-    if(i==n) return;
-    sum+=a[i];
-    goToIndex(i+1,n);
+int a[100000], sum;
+
+void goToIndex(int i, int n)
+{
+    if (i == n)
+    {
+        return;
+    }
+    sum += a[i];
+    goToIndex(i + 1, n);
 }
 
-int main(){
+int main()
+{
     int n;
-    scanf("%d",&n);
+    scanf("%d", &n);
 
-    for(int i=0;i<n;i++){
-        scanf("%d",&a[i]);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
     }
-    sum=0;
-    goToIndex(0,n);
+
+    // Use the global sum variable
+    sum = 0;
+    goToIndex(0, n);
+
+    printf("%d\n", sum);
+
+    // At this point, `sum` holds the total of the array elements
+    // You can use `sum` as needed
+
     return 0;
 }
