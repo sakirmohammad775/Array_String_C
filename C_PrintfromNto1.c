7e931ba04b6ede4de318e7a5d9d9c8bc8f7ea80f
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-11-05 22:34:56 +0600

    add recursion

---
+++ C_PrintfromNto1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int recursion(int N){
+    //reverse the 4 3 2 1
+    for (int i = N; i >=1;i--){
+        if(i>1){
+        printf("%d ", i);
+    }
+    else{
+        printf("%d", i);
+    }
+    }
+    return 0;   
+}
