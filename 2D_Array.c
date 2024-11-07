4167eabfa0c5c4a827436e210e2ccb8ee1e465c7
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-11-07 11:58:07 +0600

    add 2D Array

--- 2D_Array.c
+++ 2D_Array.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 int main()
 {
-    int arr[5][3];
+    //input
+    int n,m;
+    scanf("%d %d", &n, &m);
+    int a[n+5][m+5];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            scanf("%d",a[i][j]);
+        }
+    }
+
+    //output
     for (int i = 0; i < 5; i++)
     {
         for (int j = 0; j < 3; j++)
